6cf8c84fc68b156e8f1dc97bdabda26d37e66dde
John Jacko <jljacko@outlook.com>
2025-08-11 15:13:41 -0600

    Routing works in Angular

--- angular-personal/src/app/app.component.ts
+++ angular-personal/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { StylesService } from '@tc/tc-ngx-general';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgClass],
+  imports: [RouterOutlet, NgClass, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
