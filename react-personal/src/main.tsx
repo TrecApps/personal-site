182f0d7f546e16fef53d3c4c6bfa9fa5d803c53e
John Jacko <jljacko@outlook.com>
2025-08-13 18:46:17 -0600

    Restored functionality to that of old React site

--- react-personal/src/main.tsx
+++ react-personal/src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Provider } from 'react-redux';
+import { store } from './services/state-manager.ts';
+import { BrowserRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+          
+        </Provider>
+    
   </StrictMode>,
 )
