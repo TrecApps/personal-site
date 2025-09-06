3182ffee15027b74def55e1a764af3d4e1c53a6e
John Jacko <jljacko@outlook.com>
2025-09-06 15:04:14 -0600

    Updated library, Added Markdown Editor to Games Tab

--- angular-personal/src/app/components/game/game.component.ts
+++ angular-personal/src/app/components/game/game.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { TabComponent, TabOption } from '@tc/tc-ngx-general';
+import { MarkdownEditorComponent, TabComponent, TabOption } from '@tc/tc-ngx-general';
 import { TrooperMatchComponent } from '../games/trooper-match/trooper-match.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [TabComponent, TrooperMatchComponent, CommonModule],
+  imports: [TabComponent, TrooperMatchComponent, MarkdownEditorComponent ,CommonModule],
+
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
@@ -15,10 +16,16 @@ export class GameComponent {
     {
       showTitle: "Trooper Match",
       actTitle: "tm"
+    },
+    {
+      showTitle: "Markdown Editor",
+      actTitle: "markdown"
     }
   ]
 
   activeTab: string = "tm";
 
-
+  onSelectTab(actTitle: string){
+    this.activeTab = actTitle;
+  }
 }
